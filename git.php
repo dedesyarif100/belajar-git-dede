4035f92b4dd910a9bbae8f0dd09ba00e9b6d41cf
DEDE SYARIFUDIN HIDAYAT <Mrz_dede@yahoo.co.id>
2019-05-03 11:11:56 +0700

    modify index.html

--- git.php
+++ git.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Belajar Git</title>
 </head>
 <body>
-
+	<h1>Belajar GIT</h1>
+	<h1>Ubah File COde</h1>
+	<h1>Lalu Commit -m</h1>
+	<h1>Lalu push Origin Master</h1>
 </body>
 </html>
